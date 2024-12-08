7403ef095cfebf0c0a1e8c781425515ff7027b55
itsmraga-hub <itsragamit@gmail.com>
2024-12-08 22:01:33 +0300

    Add BottomSheet component

--- app/src/main/java/com/example/navs_topical/ui/components/BottomSheetComponent.kt
+++ app/src/main/java/com/example/navs_topical/ui/components/BottomSheetComponent.kt
@@ -1,2 +1,58 @@
 package com.example.navs_topical.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.navs_topical.verses.data.Verse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetComponent(
+    sheetState: SheetState,
+    scope: CoroutineScope,
+    dismissModal: () -> Unit,
+    verse: Verse,
+    modifier: Modifier =Modifier) {
+
+
+    ModalBottomSheet(onDismissRequest = { /*TODO*/
+            dismissModal()
+        },
+        sheetState = sheetState,
+        modifier = Modifier.fillMaxHeight(1F)
+    ) {
+
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Button(onClick = {
+                scope.launch { sheetState.hide() }.invokeOnCompletion {
+                    if (!sheetState.isVisible) {
+                        dismissModal()
+                    }
+                }
+            }) {
+                Text("Close")
+            }
+            TextField(value = verse.verseTopic, onValueChange = {})
+            TextField(value = verse.verseReference, onValueChange = {})
+            TextField(value = verse.verseContent, onValueChange = {})
+            TextField(value = verse.verseVersion, onValueChange = {})
+            TextField(value = verse.verseTag, onValueChange = {})
+        }
+    }
+}
