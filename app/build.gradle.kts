d01da45cb6f4c433e78d2161fecc697395290650
itsmraga-hub <itsragamit@gmail.com>
2024-12-23 10:50:55 +0300

    Install packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    //noinspection GradleDependency
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -67,4 +68,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.google.code.gson:gson:2.9.1")
+//    implementation("plugin.serialization:1.0.0")
 }
