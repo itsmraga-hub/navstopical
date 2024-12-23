645aa1e847d7b90da5fee693bbb22ebf2b4ba9ae
itsmraga-hub <itsragamit@gmail.com>
2024-12-23 10:54:58 +0300

    Work on reading json file from assets folder

--- app/src/main/java/com/example/navs_topical/ui/HomeScreen.kt
+++ app/src/main/java/com/example/navs_topical/ui/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.navs_topical.ui
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -21,19 +20,60 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navs_topical.ui.components.BottomSheetComponent
 import com.example.navs_topical.verses.VerseList
 import com.example.navs_topical.verses.data.Verse
+import com.example.navs_topical.verses.data.parseJsonToModel
+import com.example.navs_topical.verses.data.readJsonFromAssets
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    var sheetState = rememberModalBottomSheetState()
+    // val asvBible = readJsonFile("asv.json");
+    // val json = context.assets.open("asv.json").bufferedReader().use { it.readText() }
+//    val inputStream = context.resources.openRawResource(R.raw.asv)
+//    val json = inputStream.bufferedReader().use { it.readText() }
+
+    // val asvBible = File("src/main/resources/asv.json").readText()
+//    // println(asvBible);
+//    val json = Json { ignoreUnknownKeys = true }
+//    val bibleData: BibleData = json.decodeFromString(asvBible)
+
+
+    // Access metadata
+//    println("Name: ${bibleData.metadata.name}")
+//    println("Year: ${bibleData.metadata.year}")
+
+    // Access verses
+
+//    bibleData.verses.forEach { verse ->
+//        println("Book: ${verse.book_name}, Chapter: ${verse.chapter}, Verse: ${verse.verse}")
+//        println("Text: ${verse.text}")
+//    }
+
+    val context = LocalContext.current
+    val jsonString = readJsonFromAssets(context, "asv.json")
+    val asvBible = parseJsonToModel(jsonString)
+    println("jsonSting: $jsonString")
+    println("asvBible: $asvBible")
+    println("asvBible Metadata: ${asvBible.metadata}")
+    println("asvBible Name: ${asvBible.metadata.name}")
+    println("asvBible Verses: ${asvBible.verses}")
+    println("asvBible book: ${asvBible.verses[0].book}")
+    println("asvBible Book Name: ${asvBible.verses[0].book_name}")
+    println("asvBible Verse: ${asvBible.verses[0].verse}")
+    println("asvBible Text: ${asvBible.verses[0].text}")
+    println("asvBible Chapter: ${asvBible.verses[0].chapter}")
+
+
+    // println(asvBible?.metadata.name);
+    val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     var showBottomSheet by remember { mutableStateOf(false) }
-    var newVerse: Verse = Verse(
+    val newVerse: Verse = Verse(
         verseTopic = "",
         verseReference = "",
         verseContent = "",
