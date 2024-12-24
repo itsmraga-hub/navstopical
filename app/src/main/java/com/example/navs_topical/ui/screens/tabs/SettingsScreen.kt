0b16324816b081ba122187584ec5ab4eee4fb173
itsmraga-hub <itsragamit@gmail.com>
2024-12-24 09:57:05 +0300

    Add SettingsScreen Screen

---
+++ app/src/main/java/com/example/navs_topical/ui/screens/tabs/SettingsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.navs_topical.ui.screens.tabs
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SettingsScreen(modifier: Modifier = Modifier) {
+    Text(text = "Settings Screen", style = MaterialTheme.typography.headlineMedium)
+}
