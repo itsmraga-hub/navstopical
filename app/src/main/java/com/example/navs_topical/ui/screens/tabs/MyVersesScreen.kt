8e23a2ff33397e426e0af7c69e52bc215bc8c605
itsmraga-hub <itsragamit@gmail.com>
2024-12-26 14:20:59 +0300

    Render Bible

--- app/src/main/java/com/example/navs_topical/ui/screens/tabs/MyVersesScreen.kt
+++ app/src/main/java/com/example/navs_topical/ui/screens/tabs/MyVersesScreen.kt
@@ -4,9 +4,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.example.navs_topical.verses.data.parseJsonToModel
+import com.example.navs_topical.verses.data.readJsonFromAssets
 
 
 @Composable
 fun MyVersesScreen(modifier: Modifier = Modifier) {
     Text(text = "MyVerses Screen", style = MaterialTheme.typography.headlineMedium)
+
+    val context = LocalContext.current
+    val jsonString = readJsonFromAssets(context, "asv.json")
+    val asvBible = parseJsonToModel(jsonString)
+//    BibleViewer(bible = asvBible, bookName = "Matthew", chapter = 1)
 }
