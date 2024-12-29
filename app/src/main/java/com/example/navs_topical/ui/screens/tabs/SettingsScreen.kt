38e80ea1ee4cde2b362a9de8c32012ce0e16659e
itsmraga-hub <itsragamit@gmail.com>
2024-12-29 14:20:59 +0300

    Modify Settings Screen

--- app/src/main/java/com/example/navs_topical/ui/screens/tabs/SettingsScreen.kt
+++ app/src/main/java/com/example/navs_topical/ui/screens/tabs/SettingsScreen.kt
@@ -1,11 +1,18 @@
 package com.example.navs_topical.ui.screens.tabs
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.navs_topical.ui.screens.verses.VerseSelectionScreen
 
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier) {
-    Text(text = "Settings Screen", style = MaterialTheme.typography.headlineMedium)
+
+    Box (modifier = modifier) {
+        Text(text = "Settings Screen", style = MaterialTheme.typography.headlineMedium)
+
+        VerseSelectionScreen()
+    }
 }
