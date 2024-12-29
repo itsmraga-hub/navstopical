fb8cbcb2e0e325fc3a8521c8f22831fe4ef71837
itsmraga-hub <itsragamit@gmail.com>
2024-12-29 14:21:09 +0300

    Update MyVersesScreen

--- app/src/main/java/com/example/navs_topical/ui/screens/tabs/MyVersesScreen.kt
+++ app/src/main/java/com/example/navs_topical/ui/screens/tabs/MyVersesScreen.kt
@@ -1,20 +1,26 @@
 package com.example.navs_topical.ui.screens.tabs
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import com.example.navs_topical.verses.data.parseJsonToModel
-import com.example.navs_topical.verses.data.readJsonFromAssets
+import com.example.navs_topical.ui.screens.verses.AddVersesToMemorization
+//import com.example.navs_topical.verses.data.parseJsonToModel
+//import com.example.navs_topical.verses.data.readJsonFromAssets
 
 
 @Composable
 fun MyVersesScreen(modifier: Modifier = Modifier) {
-    Text(text = "MyVerses Screen", style = MaterialTheme.typography.headlineMedium)
 
     val context = LocalContext.current
-    val jsonString = readJsonFromAssets(context, "asv.json")
-    val asvBible = parseJsonToModel(jsonString)
+//    val jsonString = readJsonFromAssets(context, "asv.json")
+//    val asvBible = parseJsonToModel(jsonString)
 //    BibleViewer(bible = asvBible, bookName = "Matthew", chapter = 1)
+
+    Box(modifier = modifier) {
+        Text(text = "MyVerses Screen", style = MaterialTheme.typography.headlineMedium)
+        AddVersesToMemorization()
+    }
 }
