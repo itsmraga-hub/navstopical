2c242578ee3be0c50fa644b3e369462ab2133a7c
itsmraga-hub <itsragamit@gmail.com>
2024-12-29 14:42:17 +0300

    Use new CustomizedBottomAppBar

--- app/src/main/java/com/example/navs_topical/ui/screens/tabs/MyVersesScreen.kt
+++ app/src/main/java/com/example/navs_topical/ui/screens/tabs/MyVersesScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import com.example.navs_topical.ui.components.SaveDataToExternalStorage
 import com.example.navs_topical.ui.screens.verses.AddVersesToMemorization
 //import com.example.navs_topical.verses.data.parseJsonToModel
 //import com.example.navs_topical.verses.data.readJsonFromAssets
@@ -15,12 +16,14 @@ import com.example.navs_topical.ui.screens.verses.AddVersesToMemorization
 fun MyVersesScreen(modifier: Modifier = Modifier) {
 
     val context = LocalContext.current
+    val message = ""
 //    val jsonString = readJsonFromAssets(context, "asv.json")
 //    val asvBible = parseJsonToModel(jsonString)
 //    BibleViewer(bible = asvBible, bookName = "Matthew", chapter = 1)
 
     Box(modifier = modifier) {
-        Text(text = "MyVerses Screen", style = MaterialTheme.typography.headlineMedium)
-        AddVersesToMemorization()
+//        Text(text = "MyVerses Screen", style = MaterialTheme.typography.headlineMedium)
+//        AddVersesToMemorization()
+        SaveDataToExternalStorage(context = context, msg = message)
     }
 }
