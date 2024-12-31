556c41fa8f4b1123b90a022e100b8320e8c0f7c0
itsmraga-hub <itsragamit@gmail.com>
2024-12-31 11:15:25 +0300

    Create CustomTab

--- app/src/main/java/com/example/navs_topical/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/navs_topical/ui/screens/HomeScreen.kt
@@ -1,18 +1,14 @@
 package com.example.navs_topical.ui.screens
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
@@ -20,11 +16,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -33,9 +27,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,7 +39,7 @@ import com.example.navs_topical.ui.screens.tabs.ASVBibleScreen
 import com.example.navs_topical.ui.screens.tabs.MyVersesScreen
 import com.example.navs_topical.ui.screens.tabs.SettingsScreen
 import com.example.navs_topical.ui.screens.tabs.TMSScreen
-import com.example.navs_topical.verses.VerseList
+import com.example.navs_topical.verses.data.CustomTab
 import com.example.navs_topical.verses.data.Verse
 import com.example.navs_topical.verses.data.parseJsonToModel
 import com.example.navs_topical.verses.data.readJsonFromAssets
@@ -102,7 +95,13 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 tonalElevation = 8.dp
 
             ) {
-                val tabs = listOf("My Verses", "TMS", "ASV", "Settings", "More")
+
+                val tabs = listOf(
+                    CustomTab("My", Icons.Rounded.List),
+                    CustomTab("TMS", Icons.Default.CheckCircle),
+                    CustomTab("ASV", Icons.Rounded.Home),
+                    CustomTab("", Icons.Rounded.Settings),
+                    CustomTab("", Icons.Rounded.MoreVert))
 
                 CustomizedBottomAppBar(
                     tabs = tabs,
