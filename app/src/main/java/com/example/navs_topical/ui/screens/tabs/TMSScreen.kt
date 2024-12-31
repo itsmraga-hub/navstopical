9d1bb1789debae7c89eac42c895e20e7d4d89140
itsmraga-hub <itsragamit@gmail.com>
2024-12-31 15:23:52 +0300

    MEthod to read data to externalStorage

--- app/src/main/java/com/example/navs_topical/ui/screens/tabs/TMSScreen.kt
+++ app/src/main/java/com/example/navs_topical/ui/screens/tabs/TMSScreen.kt
@@ -5,25 +5,24 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.example.navs_topical.verses.VerseList
+import com.example.navs_topical.verses.TMSVerseList
 import com.example.navs_topical.verses.data.Verse
+import com.example.navs_topical.verses.data.parseJsonToModelTMS
+import com.example.navs_topical.verses.data.readJsonFromAssets
 
 
 @Composable
 fun TMSScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val tms = readJsonFromAssets(context, "tms.json")
+    val parsedTMS = parseJsonToModelTMS(tms)
+    println("TMS: $tms")
+    println("TMS: $parsedTMS")
     Text(text = "TMS Screen", style = MaterialTheme.typography.headlineMedium)
-    val dummyVerses = List(5) {
-        Verse(
-            verseTopic = "Topic $it",
-            verseVersion = "Version $it",
-            verseReference = "Reference $it",
-            verseContent = "This is the content of verse $it",
-            verseTag = "Tag $it"
-        )
-    }
-    VerseList(
-        verses = dummyVerses,
+    TMSVerseList(
+        verses = parsedTMS.c,
         modifier = Modifier.padding(16.dp)
     )
 }
